0305ff5eddc1bffbb144e0f928165fbb53846ee4
Vaimer9 <vaim3r9@gmail.com>
2022-11-14 00:18:50 +0530

    Added a proper homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+export default {
+    markdown: {
+        shikiConfig: {
+            theme: 'palenight',
+            langs: ['rust'],
+            wrap: true,
+        },
+    },
+}
